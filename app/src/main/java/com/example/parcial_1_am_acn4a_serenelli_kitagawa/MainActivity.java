684c3bc83e7d744ac41d273992a5d3517bf4f374
marinoserenelli <marino.serenelli@davinci.edu.ar>
2023-10-23 20:02:10 -0300

    Evento

--- app/src/main/java/com/example/parcial_1_am_acn4a_serenelli_kitagawa/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_serenelli_kitagawa/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.parcial_1_am_acn4a_serenelli_kitagawa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt1_siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt1_siguiente = (Button) findViewById(R.id.siguiente);
+
+        bt1_siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Proxima entrega",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
